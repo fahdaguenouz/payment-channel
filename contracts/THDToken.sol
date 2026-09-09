c4dacf0f8ff7efee01f15396f16e110fbf2a70f1
fahdaguenouz <faguenouz@gmail.com>
2026-09-09 17:08:00 +0100

    creating the contract token

---
+++ contracts/THDToken.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract THDToken {
+    string public constant name = "Thunder Token";
+    string public constant symbol = "THD";
+    uint8 public constant decimals = 18;
+
+    uint256 public totalSupply;
+    address public immutable owner;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 value) external {
+        require(msg.sender == owner, "only owner");
+        require(to != address(0), "zero address");
+        totalSupply += value;
+        balanceOf[to] += value;
+        emit Transfer(address(0), to, value);
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        uint256 permitted = allowance[from][msg.sender];
+        require(permitted >= value, "insufficient allowance");
+        if (permitted != type(uint256).max) {
+            allowance[from][msg.sender] = permitted - value;
+        }
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0), "zero address");
+        require(balanceOf[from] >= value, "insufficient balance");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+    }
+}
